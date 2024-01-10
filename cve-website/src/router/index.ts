84db57fed91ae29d979c7dde5f58e2f06c7cbab1
Thu A. Tran <ttran@mitre.org>
2024-01-10 14:46:50 -0500

    #1602 add bulma styling, axios, home page, and remove starter styling and files

--- cve-website/src/router/index.ts
+++ cve-website/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Home from '../views/Home.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: Home
     },
     {
       path: '/about',
